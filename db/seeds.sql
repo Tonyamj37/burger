96ab5dee558e92f65e152b812667183179e99972
Tonya Johnson <tmj51@zips.uakron.edu>
2019-12-12 18:34:26 -0500

    github rep

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Seed Burgers into Database ... 
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES  ("Veggie Burger", false, "2016-11-05 04:20:00"),
+        ("Cheezburger", false, "2016-11-05 04:20:02"),
+        ("Hamburger", false, "2016-11-05 04:20:20");
+  
